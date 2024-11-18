e339127ea2b7eaf6f53fd90103bb87653ca5a658
Anderson Alves de Oliveira Mares <andersonmares@gmail.com>
2024-11-18 14:55:53 -0300

    Conclindo o about us

--- desafio-advocacia/src/app/about-us/about-us.component.ts
+++ desafio-advocacia/src/app/about-us/about-us.component.ts
@@ -8,4 +8,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css'],
 })
-export class AboutUsComponent {}
+export class AboutUsComponent {
+  team = [
+    {
+      name: 'Dr. João Silva',
+      description: 'Especialista em Direito Empresarial.',
+      image: 'assets/joao.jpg',
+    },
+    {
+      name: 'Dra. Maria Oliveira',
+      description: 'Especialista em Direito Civil.',
+      image: 'assets/maria.jpg',
+    },
+    {
+      name: 'Dr. Carlos Souza',
+      description: 'Especialista em Direito Penal.',
+      image: 'assets/carlos.jpg',
+    },
+  ];
+}
