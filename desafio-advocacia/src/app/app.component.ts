d827020ed5e1f78cd9df701e568bc3c17d7eeb37
Anderson Alves de Oliveira Mares <andersonmares@gmail.com>
2024-11-17 22:05:40 -0300

    Definindo as rotas e ajustando o form de contato

--- desafio-advocacia/src/app/app.component.ts
+++ desafio-advocacia/src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { CarouselComponent } from "./carousel/carousel.component";
-import { ContactFormComponent } from "./contact-form/contact-form.component";
-import { AboutUsComponent } from "./about-us/about-us.component";
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, CarouselComponent, ContactFormComponent, AboutUsComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HeaderComponent,
+    CarouselComponent,
+    ContactFormComponent,
+    AboutUsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'desafio-advocacia';
--- desafio-advocacia/src/app/app.routes.ts
+++ desafio-advocacia/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { FaqComponent } from './faq/faq.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { NgModule } from '@angular/core';
+import { CarouselComponent } from './carousel/carousel.component'; // Importe o componente
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'carousel', component: CarouselComponent }, // Rota para o Carousel
+  { path: 'faq', component: FaqComponent },
+  { path: 'sobre-nos', component: AboutUsComponent },
+  { path: 'contato', component: ContactFormComponent },
+  { path: '', redirectTo: '/carousel', pathMatch: 'full' }, // Redireciona a rota inicial para o Carousel
+  { path: '**', redirectTo: '/carousel' }, // Redireciona rotas inválidas para o Carousel
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
